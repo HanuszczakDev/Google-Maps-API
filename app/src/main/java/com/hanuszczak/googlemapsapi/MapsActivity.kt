6b4632deb2f278ebdb9db58e647e84c9672c6164
ahanuszczak <hanyhusajn@gmail.com>
2023-04-03 14:03:50 +0200

    POI implementation - point of interest click listener

--- app/src/main/java/com/hanuszczak/googlemapsapi/MapsActivity.kt
+++ app/src/main/java/com/hanuszczak/googlemapsapi/MapsActivity.kt
@@ -61,6 +61,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.addMarker(MarkerOptions().position(homeLatLng))
 
         setMapLongClick(map)
+        setPoiClick(map)
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker?.showInfoWindow()
+        }
     }
 
     private fun setMapLongClick(map: GoogleMap) {
