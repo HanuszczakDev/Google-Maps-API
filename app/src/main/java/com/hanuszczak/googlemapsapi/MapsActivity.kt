62966136cae90ce629533d73e72a319fe2fd1588
ahanuszczak <hanyhusajn@gmail.com>
2023-04-03 14:09:49 +0200

    custom map style

--- app/src/main/java/com/hanuszczak/googlemapsapi/MapsActivity.kt
+++ app/src/main/java/com/hanuszczak/googlemapsapi/MapsActivity.kt
@@ -1,7 +1,9 @@
 package com.hanuszczak.googlemapsapi
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
@@ -10,11 +12,13 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.hanuszczak.googlemapsapi.databinding.ActivityMapsBinding
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val TAG = MapsActivity::class.java.simpleName
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
@@ -62,6 +66,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         setMapLongClick(map)
         setPoiClick(map)
+        setMapStyle(map)
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this,
+                    R.raw.map_style
+                )
+            )
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
     }
 
     private fun setPoiClick(map: GoogleMap) {
